3f39aea948006df33e80506017052b49ced4c07c
kimotot <god4bid@hear.to>
2017-05-11 11:39:05 +0900

    Min and Max 組込関数を自作する問題 generator対応が難しかった

--- min_max.py
+++ min_max.py
@@ -1,8 +1,46 @@
-def min(*args, **kwargs):
+def comparison(*args, **kwargs):
     key = kwargs.get("key", None)
-    return None
+    f = kwargs.get("func", None)
+
+    if len(args) == 1:
+        it = args[0]
+    else:
+        it = args
+
+    result = None
+
+    if key is None:
+        for i in it:
+            if result is None:
+                result = i
+            else:
+                if f(result, i):
+                    pass
+                else:
+                    result = i
+        return result
+    else:
+        for i in it:
+            if result is None:
+                result = i
+            else:
+                if f(key(result), key(i)):
+                    pass
+                else:
+                    result = i
+        return result
+
+
+def max(*args, **kwargs):
+    kwargs["func"] = lambda a, b: a >= b
+    return comparison(*args, **kwargs)
+
+
+def min(*args, **kwargs):
+    kwargs["func"] = lambda a, b: a <= b
+    return comparison(*args, **kwargs)
 
 
 if __name__ == '__main__':
 
-    min([1,2,3], key=lambda x: x[1])
+    print(min(abs(i) for i in range(-10, 10)))
